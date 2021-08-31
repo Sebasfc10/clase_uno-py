3f1a7157324af62e222bdbf2061db8fc17f7617d
Jean Sebastian Torres L <jtorres68@cuc.edu.co>
2021-08-30 19:22:42 -0500

    segundo commit procesos aritmeticos

--- clase_uno.py
+++ clase_uno.py
@@ -1,2 +1,31 @@
 # -*- coding: utf-8 -*-
+import math
 
+# print('hi world')
+
+
+# VARIABLES
+
+a = 3
+print(type(a))
+
+a = 'gg'
+print(type(a))
+
+# potencia 
+
+a = 5
+b = 2
+c = a ** b
+print(c)
+
+# raiz
+
+a = 27
+c = a ** (1/3)
+print(c)
+
+
+
+raiz = math.sqrt(25)
+print(raiz)
