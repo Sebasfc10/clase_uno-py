2c6a890a39cd77e267afbaf422836e5baab446e1
Jean Sebastian Torres L <jtorres68@cuc.edu.co>
2021-08-30 20:55:08 -0500

    captura por pantalla y ejercicio condicionales

--- clase_uno.py
+++ clase_uno.py
@@ -33,3 +33,49 @@ a = '3'
 y = int(a) #vale para float, str, int, etc...
 print(y)
 print(type(y))
+
+# capturar por screen
+
+nombre = input('digite su nombre:')
+print('hola',nombre)
+
+n1 = input('ingresa el primer numero: ')
+n1 = int(n1)
+n2 = input('ingresa el segundo numero: ')
+n2 = int(n2)
+suma= n1 + n2
+
+print(suma)
+
+# segunda tarea
+
+numero = int(input('digite el numero que se elevara al cuadrado: '))
+cuadrado = numero ** 2
+print(f'el numero elevado es {numero} y elevado da : {cuadrado}')
+
+# tarea 3
+
+precio = float(input('digite el precio a descontar: '))
+descuento = precio * 20/100
+ahorrado = precio - descuento
+print(f'el valor inicial es: {precio}')
+print(f'el valor ahorrado es: {ahorrado}')
+print(f'el valor en descuento es este: {descuento}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
